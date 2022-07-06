13dbfa183616a06313fcaa1b7fa290b5639c69f1
bngams <n.boris@devidia.net>
2022-07-06 16:23:37 +0200

    first commit with template syntax examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  today = new Date();
+  svgBgColor = 'dark';
+  imgUrl = 'https://picsum.photos/200/300?random=1';
+  imgName = 'Lorem picsum';
+
+  constructor() {
+  }
+
+  clickMe(event: Event) {
+    console.log('clickMe event : ', event)
+  }
+
+  toggleIconColor() {
+    if(this.svgBgColor == 'dark') {
+      this.svgBgColor = 'red';
+    } else{
+      this.svgBgColor = 'dark';
+    }
+  }
+}
+
+@Component({
+  selector: 'app-sub-root',
+  template: `
+    <h1>Welcome</h1>
+  `,
+  styles: [`
+    h1 { color: red }
+  `]
+})
+export class AppSubComponent {
+  title = 'my-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, AppSubComponent } from './app.component';
+import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppSubComponent,
+    ExponentialStrengthPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // exports: [
+  //   ExponentialStrengthPipe
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
